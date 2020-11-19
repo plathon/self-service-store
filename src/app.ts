0f24093aa2c81112aaeaf326d8864899dbf5e526
= <=>
2020-11-19 20:53:19 -0300

    docs: config api docs placeholder

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from 'express'
 
+import swaggerUI from 'swagger-ui-express'
+import swaggerDocument from './api-schema.json'
+
 const server = express()
 
 server.use(express.json())
@@ -7,6 +10,8 @@ server.use(express.json())
 server.get('/', (request: Request, response: Response) => {
   response.status(200).json({ response: 'ok!!' })
 })
+
+server.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 const port = 3000
 const host = '0.0.0.0'
